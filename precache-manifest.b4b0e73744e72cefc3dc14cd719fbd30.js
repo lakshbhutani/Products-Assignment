99b304cf8ecc6e27013c1cb72d106db959b76967
laksh bhutani <laksh.bhutani@kelltontech.com>
2020-10-11 17:08:14 +0530

    Updates

---
+++ precache-manifest.b4b0e73744e72cefc3dc14cd719fbd30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a88b6a56023da93fac4a9a98d0d1a5eb",
+    "url": "/Products-Assignment/index.html"
+  },
+  {
+    "revision": "2b6c402ce5fb5de7c520",
+    "url": "/Products-Assignment/static/css/main.cff400de.chunk.css"
+  },
+  {
+    "revision": "cef70b2f06b1ee3812e1",
+    "url": "/Products-Assignment/static/js/2.39d4c1e1.chunk.js"
+  },
+  {
+    "revision": "2b6c402ce5fb5de7c520",
+    "url": "/Products-Assignment/static/js/main.e0565627.chunk.js"
+  },
+  {
+    "revision": "d301af49b2c8a37d00a0",
+    "url": "/Products-Assignment/static/js/runtime~main.51c4e82d.js"
+  },
+  {
+    "revision": "7ed3f404ab7a1c00dc470f0b9616bfaf",
+    "url": "/Products-Assignment/static/media/nykaa_logo.7ed3f404.svg"
+  }
+]);
